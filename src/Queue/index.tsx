ee01abb2edb2fbfd4a07322291f40ba5a3afddce
caiquedegaspari <caique.degaspare@gmail.com>
2023-06-09 08:48:14 -0300

    feat: add queue implementation/styles

--- src/Queue/index.tsx
+++ src/Queue/index.tsx
@@ -1,5 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {
+	ButtonGroupContainer,
+	Container,
+	DefaultItem as QueueItem,
+} from '../styled';
+import { QueueContainer, TitleContainer } from './styled';
 
 export function Queue() {
-	return <h1>Queue</h1>;
+	const [queue, setQueue] = useState<string[]>([]);
+
+	const enqueue = () => {
+		setQueue([...queue, `Item ${queue.length}`]);
+	};
+
+	const dequeue = () => {
+		const toDequeue = queue;
+		toDequeue.shift();
+		setQueue([...toDequeue]);
+	};
+
+	const front = () => {
+		alert(queue[0]);
+	};
+
+	const end = () => {
+		alert(queue[queue.length - 1]);
+	};
+
+	const size = () => {
+		alert(`Queue size is: ${queue.length}`);
+	};
+
+	return (
+		<Container>
+			<div>
+				<h1>Actions</h1>
+				<ButtonGroupContainer>
+					<button onClick={enqueue}>Enqueue</button>
+					<button onClick={dequeue}>Dequeue</button>
+					<button onClick={front}>Queue Front</button>
+					<button onClick={end}>Queue End</button>
+					<button onClick={size}>Size</button>
+				</ButtonGroupContainer>
+			</div>
+			<TitleContainer>
+				<h1>Queue</h1>
+				<QueueContainer>
+					{queue
+						.map((item, key) => (
+							<QueueItem key={key}>
+								<p>{item}</p>
+							</QueueItem>
+						))
+						.reverse()}
+				</QueueContainer>
+			</TitleContainer>
+		</Container>
+	);
 }
--- src/Queue/styled.ts
+++ src/Queue/styled.ts
@@ -1,3 +1,14 @@
 import styled from 'styled-components';
 
-export {};
+const QueueContainer = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	width: 600px;
+`;
+
+const TitleContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+`;
+
+export { QueueContainer, TitleContainer };
